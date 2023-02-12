ec2a0c7990c54eb16462b9bc88a4685e1050136e
Alem Haile <wellma0043@gmail.com>
2023-02-12 04:24:28 +0300

    fix with linux

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request-promise-native');
+
+async function main(id) {
+  try {
+    const movieData = await request(`https://swapi-api.alx-tools.com/api/films/${id}/`);
+    const movie = JSON.parse(movieData);
+
+    const promises = movie.characters.map(async (character) => {
+      const characterData = await request(character);
+      const char = JSON.parse(characterData);
+      return char.name;
+    });
+
+    const characters = await Promise.all(promises);
+    characters.forEach(char => console.log(char));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main(process.argv[2]);
