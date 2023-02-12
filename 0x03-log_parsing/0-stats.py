95d05e15c1e86f0b9b0071da28932253418ad12b
Alem Haile <wellma0043@gmail.com>
2023-02-12 01:18:00 -0800

    fixed log parsing 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """
-Log parsing
+Log Parsing
+
+This script parses log data from the standard input and prints metrics about the log data.
+The metrics include the total file size and the count of each status code.
+
+The script counts the number of lines of log data read from the standard input and
+prints the metrics every 10 lines. If a KeyboardInterrupt (e.g. `Ctrl + C`) is received,
+the metrics are printed and the script terminates.
 """
 import sys
 
-
 def print_metrics(file_size, status_codes):
     """
-    Print metrics
+    Print the log metrics
+
+    Arguments:
+    - file_size: the total file size of the log data
+    - status_codes: a dictionary with the count of each status code
     """
     print("File size: {}".format(file_size))
     codes_sorted = sorted(status_codes.keys())
@@ -15,14 +25,23 @@ def print_metrics(file_size, status_codes):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
 
-
-codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
+# Initialize the count of each status code
+codes_count = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 file_size_total = 0
 count = 0
 
 if __name__ == "__main__":
     try:
+        # Parse log data from standard input
         for line in sys.stdin:
             try:
                 status_code = line.split()[-2]
@@ -41,4 +60,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_metrics(file_size_total, codes_count)
         raise
-   print_metrics(file_size_total, codes_count)
+    print_metrics(file_size_total, codes_count)
