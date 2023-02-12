4c82877c81ff4ea0d6ff3f7c79ef29d738c9aee2
Alem Haile <wellma0043@gmail.com>
2023-02-12 04:45:20 +0300

    fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,68 @@
-#!/usr/bin/python3import sysdef is_valid(board, row, col, n):    """Check if there is a queen in the same column or diagonal."""    # Check if there is a queen in the same column    for i in range(row):        if board[i][col] == 1:            return False    # Check the upper left diagonal    i, j = row, col    while i >= 0 and j >= 0:        if board[i][j] == 1:            return False        i -= 1        j -= 1    # Check the upper right diagonal    i, j = row, col    while i >= 0 and j < n:        if board[i][j] == 1:            return False        i -= 1        j += 1    return Truedef solve_n_queens(board, row, n, results):    """Find all the solutions to the N queens puzzle."""    if row == n:        result = []        for i in range(n):            result.append([i, board[i].index(1)])        results.append(result)        return    for i in range(n):        if is_valid(board, row, i, n):            board[row][i] = 1            solve_n_queens(board, row + 1, n, results)            board[row][i] = 0def main():    """Main function to parse command line arguments and run the program."""    if len(sys.argv) != 2:        print("Usage: nqueens N")        sys.exit(1)    try:        n = int(sys.argv[1])    except ValueError:        print("N must be a number")        sys.exit(1)    if n < 4:        print("N must be at least 4")        sys.exit(1)    board = [[0 for _ in range(n)] for _ in range(n)]    results = []    solve_n_queens(board, 0, n, results)    for result in results:        print(result)if __name__ == "__main__":    main()
+#!/usr/bin/python3
+"""
+Solution to the nqueens problem
+"""
+import sys
+
+
+def backtrack(r, n, cols, pos, neg, board):
+    """
+    backtrack function to find solution
+    """
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            for k in range(len(board[l])):
+                if board[l][k] == 1:
+                    res.append([l, k])
+        print(res)
+        return
+
+    for c in range(n):
+        if c in cols or (r + c) in pos or (r - c) in neg:
+            continue
+
+        cols.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r+1, n, cols, pos, neg, board)
+
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] = 0
+
+
+def nqueens(n):
+    """
+    Solution to nqueens problem
+    Args:
+        n (int): number of queens. Must be >= 4
+    Return:
+        List of lists representing coordinates of each
+        queen for all possible solutions
+    """
+    cols = set()
+    pos_diag = set()
+    neg_diag = set()
+    board = [[0] * n for i in range(n)]
+
+    backtrack(0, n, cols, pos_diag, neg_diag, board)
+
+
+if __name__ == "__main__":
+    n = sys.argv
+    if len(n) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        nn = int(n[1])
+        if nn < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(nn)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
