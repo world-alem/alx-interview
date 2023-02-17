4674074ba051f081a7557e5a96150b72c3b194fd
Alem Haile <wellma0043@gmail.com>
2023-02-16 18:09:26 -0800

    star warts api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,17 @@
 #!/usr/bin/node
 
-const request = require('request-promise-native');
+const request = require('request');
 
-async function main (id) {
-  try {
-    const movieData = await request(`https://swapi-api.alx-tools.com/api/films/${id}/`);
-    const movie = JSON.parse(movieData);
-
-    const characters = await Promise.all(movie.characters.map(async (character) => {
-      const characterData = await request(character);
-      const char = JSON.parse(characterData);
-      return char.name;
-    }));
-
-    characters.forEach(char => console.log(char));
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-if (process.argv.length < 3) {
-  console.error('Expected at least one argument: the movie ID');
-  process.exit(1);
-}
-
-main(process.argv[2]);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
